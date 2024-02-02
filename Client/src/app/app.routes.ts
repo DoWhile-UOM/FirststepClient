18082060d5bc5afd1da3787a3ad08a5fdd2c1f9f
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-02-02 15:36:11 +0530

    route made for seeker

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SignupFirstPageComponent } from './SeekerPortal/signup-first-page/signup-first-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'seeker-portal', component: SignupFirstPageComponent},
+];
