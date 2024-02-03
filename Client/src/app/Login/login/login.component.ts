6642527df0aca5035d8b3a997603adbb5a35e1dd
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-02-04 03:22:18 +0530

    completed the basic structure of the login page. forgot password pop up has not done yet

--- Client/src/app/Login/login/login.component.ts
+++ Client/src/app/Login/login/login.component.ts
@@ -2,29 +2,37 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../../shared/nav-bar/nav-bar.component";
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
     selector: 'app-login',
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
-    imports: [NavBarComponent,FormsModule,HttpClientModule]
+    imports: [NavBarComponent,FormsModule,HttpClientModule,MatCardModule,MatFormFieldModule, MatInputModule, MatIconModule,MatButtonModule]
 })
 export class LoginComponent {
+    hide = true;
     loginObj: Login;
-    constructor(private http: HttpClient) {
+     constructor(/*private http: HttpClient*/ ) {
         this.loginObj = new Login();
     }
     onLogin(){
         debugger;
-        this.http.post('http://localhost:3000/login',this.loginObj).subscribe((res:any)=>{
-            if(res.result){
-                alert('Login Success');
-            }
-            else{
-                alert(res.message);
-            }
-        });
+        // this.http.post('http://localhost:3000/login',this.loginObj).subscribe((res:any)=>{
+        //     if(res.result){
+        //         alert('Login Success');
+        //     }
+        //     else{
+        //         alert(res.message);
+        //     }
+        // });
     }
 }
 export class Login{
