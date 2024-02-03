334a093806b6df291ab4703b07e0a7031a208a00
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-02-03 23:38:19 +0530

    loging routing completed

--- Client/src/app/Login/login/login.component.ts
+++ Client/src/app/Login/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../shared/nav-bar/nav-bar.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css',
+    imports: [NavBarComponent]
 })
 export class LoginComponent {
 
--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Login/login/login.component'; // Fix the import path
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'login', pathMatch: 'full'
+    },
+    {
+        path: 'login', 
+        component: LoginComponent
+    },
+    
+];
