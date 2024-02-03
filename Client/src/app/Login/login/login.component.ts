df0fd19ddc7edee4635bc5910877eb4cceb1ff9a
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-02-04 00:45:36 +0530

    partially created routing. But got an error. appaearently "HttpClient" is not recognized by the login component; probably because of a version case.

--- Client/src/app/Login/login/login.component.ts
+++ Client/src/app/Login/login/login.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../shared/nav-bar/nav-bar.component";
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-login',
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
-    imports: [NavBarComponent]
+    imports: [NavBarComponent,FormsModule,HttpClientModule]
 })
 export class LoginComponent {
+    loginObj: Login;
+    constructor(private http: HttpClient) {
+        this.loginObj = new Login();
+    }
+    onLogin(){
+        debugger;
+        this.http.post('http://localhost:3000/login',this.loginObj).subscribe((res:any)=>{
+            if(res.result){
+                alert('Login Success');
+            }
+            else{
+                alert(res.message);
+            }
+        });
+    }
+}
+export class Login{
+    email: string;
+  password: string;
 
+  constructor(){
+    this.email='';
+    this.password='';
+  }
 }
