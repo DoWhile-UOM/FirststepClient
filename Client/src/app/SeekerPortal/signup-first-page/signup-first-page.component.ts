135977477a646dc61cd74e0b1fc3aab4ae6457cc
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-02-05 00:25:57 +0530

    nav-bar created

--- Client/src/app/SeekerPortal/signup-first-page/signup-first-page.component.ts
+++ Client/src/app/SeekerPortal/signup-first-page/signup-first-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NavBarComponent } from '../../shared/nav-bar/nav-bar.component';
 @Component({
   selector: 'app-signup-first-page',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './signup-first-page.component.html',
   styleUrl: './signup-first-page.component.css'
 })
