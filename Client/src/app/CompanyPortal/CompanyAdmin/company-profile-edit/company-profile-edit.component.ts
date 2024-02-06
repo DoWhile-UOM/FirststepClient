e8895b877c9d32e51a098e67d04bf62182e95687
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-02-06 09:32:17 +0530

    completed form details in company profile edit component

--- Client/src/app/CompanyPortal/CompanyAdmin/company-profile-edit/company-profile-edit.component.ts
+++ Client/src/app/CompanyPortal/CompanyAdmin/company-profile-edit/company-profile-edit.component.ts
@@ -5,22 +5,31 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {CdkTextareaAutosize, TextFieldModule} from '@angular/cdk/text-field';
 import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
-interface IBScale {
-  scale: string;
-}
+
 
 @Component({
     selector: 'app-company-profile-edit',
     standalone: true,
     templateUrl: './company-profile-edit.component.html',
     styleUrl: './company-profile-edit.component.css',
-    imports: [NavBarComponent,MatInputModule,MatFormFieldModule,FormsModule,CdkTextareaAutosize,TextFieldModule,MatSelectModule]
+    imports: [NavBarComponent,MatInputModule,MatFormFieldModule,FormsModule,CdkTextareaAutosize,TextFieldModule,MatSelectModule,MatIconModule,ReactiveFormsModule,MatDividerModule,MatButtonModule]
 })
 export class CompanyProfileEditComponent {
-  scale: IBScale[] = [
-    {scaleType: 'Small',viewValue: 'Small Scale'},
-    {scaleType: 'Medium',viewValue: 'Medium Scale'},
-    {scaleType: 'Large',viewValue: 'Large Scale'},
-  ];
+  selected = 'option2';
+  selectedCity= 'option1';
+  selectedProvince= 'option1';
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
