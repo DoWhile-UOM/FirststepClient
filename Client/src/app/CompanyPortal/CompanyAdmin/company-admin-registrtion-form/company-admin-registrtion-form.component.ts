f228215f1e1211e4270dae4e6462159cc4400e82
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-02-07 19:45:53 +0530

    completed company profile edit

--- Client/src/app/CompanyPortal/CompanyAdmin/company-admin-registrtion-form/company-admin-registrtion-form.component.ts
+++ Client/src/app/CompanyPortal/CompanyAdmin/company-admin-registrtion-form/company-admin-registrtion-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../../shared/nav-bar/nav-bar.component";
-import { FormsModule } from '@angular/forms';
+import { FormsModule,FormControl,Validators,  ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
@@ -14,8 +14,17 @@ import {FlexLayoutServerModule} from '@angular/flex-layout/server';
     standalone: true,
     templateUrl: './company-admin-registrtion-form.component.html',
     styleUrl: './company-admin-registrtion-form.component.css',
-    imports: [NavBarComponent,FormsModule,HttpClientModule,MatCardModule,MatFormFieldModule, MatInputModule, MatIconModule,MatButtonModule,FlexLayoutServerModule]
+    imports: [NavBarComponent,FormsModule,HttpClientModule,MatCardModule,MatFormFieldModule, MatInputModule, MatIconModule,MatButtonModule,FlexLayoutServerModule, ReactiveFormsModule]
 })
 export class CompanyAdminRegistrtionFormComponent {
+    email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  hide = true;
 }
