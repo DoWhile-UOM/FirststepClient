222cb509989f89fb987ba3349a16981293ecb142
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-02-07 06:29:35 +0530

    created cmonapny admin registration component and made the basic structure

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Login/login/login.component'; // Fix the import path
 import { ForgotPasswordComponent } from './Login/forgot-password/forgot-password.component';
 import { CompanyProfileEditComponent } from './CompanyPortal/CompanyAdmin/company-profile-edit/company-profile-edit.component';
+import { CompanyAdminRegistrtionFormComponent } from './CompanyPortal/CompanyAdmin/company-admin-registrtion-form/company-admin-registrtion-form.component';
 
 export const routes: Routes = [
     {
@@ -18,6 +19,10 @@ export const routes: Routes = [
     {
         path: 'company-admin/profile-edit', 
         component: CompanyProfileEditComponent
+    },
+    {
+        path: 'company-registration/company-admin-registration', 
+        component: CompanyAdminRegistrtionFormComponent
     }
     
     
