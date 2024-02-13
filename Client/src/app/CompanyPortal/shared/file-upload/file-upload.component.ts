16d28e01a465f1c1df0c44b2e60bdb884934cccb
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-02-13 19:15:20 +0530

    file upload

--- Client/src/app/CompanyPortal/shared/file-upload/file-upload.component.ts
+++ Client/src/app/CompanyPortal/shared/file-upload/file-upload.component.ts
@@ -4,15 +4,19 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-file-upload',
   standalone: true,
-  imports: [MatIconModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatButtonModule],
+  imports: [MatIconModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatButtonModule,MatProgressBarModule],
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.css'
 })
 export class FileUploadComponent {
+  fileName = ''; 
+  constructor(private http: HttpClient) { }
   triggerFileInput() {
     const fileInput = document.getElementById('fileID') as HTMLInputElement;
     if (fileInput) {
@@ -25,8 +29,16 @@ export class FileUploadComponent {
     if (inputElement.files && inputElement.files.length > 0) {
       // access the selected file using inputElement.files[0]
       const selectedFile = inputElement.files[0];
+      this.fileName = selectedFile.name;
       console.log('Selected file:', selectedFile);
+
       //  uploading the file to a server.
+
+      const formData = new FormData();
+      formData.append("thumbnail", selectedFile);
+      const upload$=this.http.post("/api/thumbnail-upload", formData)
+      upload$.subscribe();
     }
   }
+   
 }
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageRolesComponent } from "./CompanyPortal/CompanyAdmin/manage-roles/manage-roles.component";
 import { SignUpSecondPageComponent } from "./SeekerPortal/sign-up-second-page/sign-up-second-page.component";
 import { AddrolesPopupComponent } from './CompanyPortal/CompanyAdmin/addroles-popup/addroles-popup.component';
+
 
 
 
@@ -16,5 +17,10 @@ import { AddrolesPopupComponent } from './CompanyPortal/CompanyAdmin/addroles-po
 })
 export class AppComponent {
   title = 'Client';
+
+
 }
+
+
+
 
