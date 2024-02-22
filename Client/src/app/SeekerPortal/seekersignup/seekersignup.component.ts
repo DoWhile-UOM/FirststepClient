44971e5b94c81566f4ad0dbe69893b4653e86fc6
RuwandieD <dharmapalard.21@uom.lk>
2024-02-22 23:23:27 +0530

    signup process completed without chips

--- Client/src/app/SeekerPortal/seekersignup/seekersignup.component.ts
+++ Client/src/app/SeekerPortal/seekersignup/seekersignup.component.ts
@@ -4,13 +4,34 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatStepperModule } from '@angular/material/stepper';
+import { FileUploadComponent } from "../../CompanyPortal/shared/file-upload/file-upload.component";
+import { JobOfferListComponent } from "../../CompanyPortal/shared/job-offer-list/job-offer-list.component";
+
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { ElementRef, ViewChild, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  MatAutocompleteSelectedEvent,
+  MatAutocompleteModule,
+} from '@angular/material/autocomplete';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { Observable, Subscribable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
-  selector: 'app-seekersignup',
-  standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatStepperModule],
-  templateUrl: './seekersignup.component.html',
-  styleUrl: './seekersignup.component.css'
+    selector: 'app-seekersignup',
+    standalone: true,
+    templateUrl: './seekersignup.component.html',
+    styleUrl: './seekersignup.component.css',
+    imports: [FormsModule, ReactiveFormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatStepperModule, FileUploadComponent, JobOfferListComponent, MatIconModule, FlexLayoutModule, MatCheckboxModule, MatAutocompleteModule, MatChipsModule, MatDividerModule, MatCardModule]
 })
 export class SeekersignupComponent {
 
@@ -33,5 +54,24 @@ fifthFormGroup = this._formBuilder.group({
 
 
 constructor(private _formBuilder: FormBuilder) { }
+
+//image
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  
+}
+url = "./assets/images/SeekerEdit.jpg";
+
+onselectFile(event: any){
+  if(event.target.files){
+    var reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (event: any) => {
+      this.url = event.target.result;
+    }
+    
+  }
+}
 
 }
