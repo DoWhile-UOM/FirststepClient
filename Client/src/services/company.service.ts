9694dba1467efd9f7dd89d3a8f94982fa70d15d8
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-03-13 18:48:48 +0530

    Reg status check started

--- Client/src/services/company.service.ts
+++ Client/src/services/company.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  //Get company Registration state details---Start
+  async getCompnayRegState(id: number) {
+    let cmpData: any;
+
+    this.http.get('https://localhost:7213/api/Company/GetCompanyById')
+      .subscribe(data => {
+        // Handle successful response with the data
+        console.log(data);
+      }, error => {
+        // Handle error scenario
+        console.error(error);
+      });
+
+    return cmpData;
+  }
+  //Get company Registration state details---End
+
+
 }
