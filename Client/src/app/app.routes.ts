c2d0f781c3aa1756971e55719583c8f95a740fbd
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-03-21 21:28:03 +0530

    Change File Structure #2

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,22 +1,21 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './components/login/login.component';
-
 import { SignupComponent } from './signup/signup.component';
-
 
 export const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
   
   {
-    path: 'company/admin',
-    loadChildren: () => import('./portals/company-admin-portal/company-admin.module').then(m => m.caRoutes),
-    // example link: http://localhost:4200/company/admin/jobOfferList
+    path: 'ca',
+    loadChildren: () => import('./routes/company-admin.routes').then(m => m.caRoutes),
+    // example link: http://localhost:4200/ca/jobOfferList
   },
 
   {
     path: 'seeker',
-    loadChildren: () => import('./portals/seeker-portal/seeker.module').then(m => m.seekerRoutes),
+    loadChildren: () => import('./routes/seeker.routes').then(m => m.routes),
     // example link: http://localhost:4200/seeker/home
   },
 ];
