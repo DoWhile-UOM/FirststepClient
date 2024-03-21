f1b9ced59286d870f0cc7eff71c86316ec0cd863
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-03-21 20:38:32 +0530

    Add all routing modules

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,18 +1,7 @@
 import { Routes } from '@angular/router';
-import { ManageRolesComponent } from './components/manage-roles/manage-roles.component';
-import { EditRoleComponent } from './components/edit-role/edit-role.component';
-import { AddrolesPopupComponent } from './components/addroles-popup/addroles-popup.component';
-import { NewJobComponent } from './components/new-job/new-job.component';
-import { FileUploadComponent } from './components/file-upload/file-upload.component';
-import { JobOfferListComponent } from './components/job-offer-list/job-offer-list.component';
 import { LoginComponent } from './components/login/login.component';
 
 import { SignupComponent } from './signup/signup.component';
-import { authGuard } from './guards/auth.guard';
-
-import { RegisterCompanyComponent } from './components/register-company/register-company.component';
-
-import { RegCmpStateCheckComponent } from './components/reg-cmp-state-check/reg-cmp-state-check.component';
 
 
 export const routes: Routes = [
@@ -30,7 +19,4 @@ export const routes: Routes = [
     loadChildren: () => import('./portals/seeker-portal/seeker.module').then(m => m.seekerRoutes),
     // example link: http://localhost:4200/seeker/home
   },
-
-  //shared
-  { path: 'file', component: FileUploadComponent },
 ];
