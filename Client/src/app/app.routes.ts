d9bf6b06e6d8790b41306d8f86cc964238a0cef6
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-03-24 18:04:39 +0530

    company registration route added

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { RegCmpStateCheckComponent } from './components/reg-cmp-state-check/reg-cmp-state-check.component';
+import { RegisterCompanyComponent } from './components/register-company/register-company.component';
 
 export const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
   {path:'RegCheck',component:RegCmpStateCheckComponent},
+  {path:'CompanyReg',component:RegisterCompanyComponent},
   
   {
     path: 'ca',
