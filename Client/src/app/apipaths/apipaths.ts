fead9b4b514840bd96a735f48c660844e7f38fc4
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-03-25 21:37:30 +0530

    manage roles changes

--- Client/src/app/apipaths/apipaths.ts
+++ Client/src/app/apipaths/apipaths.ts
@@ -39,4 +39,7 @@ export class Apipaths {
     // external
     static readonly getCountryNames: string = 'https://api.first.org/v1/get-countries';
     static readonly getCityNames: string = 'https://countriesnow.space/api/v0.1/countries/cities';
+
+    // seeker
+    static readonly getSeekerDetails: string = this.baseUrl + 'Seeker/GetSeekerById/';
 }
--- Client/src/services/seeker.service.ts
+++ Client/src/services/seeker.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { Apipaths } from '../app/apipaths/apipaths';
+import axios from 'axios';
 @Injectable({
   providedIn: 'root'
 })
 export class SeekerService {
 
-  constructor() { }
+async getSeekerDetails(id : number) {
+  let seekerData: any;
+  try{
+    await axios.get(Apipaths.getSeekerDetails + id)
+      .then((response) => {
+        seekerData = response.data;
+      });
+  }
+  catch (error) {
+    console.error(error);
+  }
+
+  return seekerData;
+}
+
 }
