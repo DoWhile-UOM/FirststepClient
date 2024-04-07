b5b7ebb56464d7970726d4cc3b9b7f21df322ca2
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2024-04-08 00:13:20 +0530

    Complete pdf viewer component

--- Client/src/app/components/pdf-viewer/pdf-viewer.component.ts
+++ Client/src/app/components/pdf-viewer/pdf-viewer.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit, Inject } from '@angular/core';
+import { Component, Input, Inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'pdf-viewer-dialog',
@@ -15,14 +17,44 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule, 
     MatCardModule, 
     NgxExtendedPdfViewerModule, 
-    CommonModule],
+    CommonModule,
+    MatIconModule],
   template: `
     <mat-card class="emptymsg">
-      <div *ngIf="pdfSrc !== ''">
-        <mat-card-header>
-          <mat-card-title>{{title}}</mat-card-title>
-        </mat-card-header>
-        <ngx-extended-pdf-viewer [src]="pdfSrc"></ngx-extended-pdf-viewer>
+      <div class="pdf-viewer-container" *ngIf="pdfSrc !== ''">
+        <div class="pdf-header">
+          <span class="title">
+              <mat-icon class="icon">picture_as_pdf</mat-icon>
+              <p class="text">{{title}}</p>
+          </span>
+
+          <span class="action-buttons">
+            <button mat-raised-button color="primary">
+              <mat-icon>cloud_download</mat-icon> Download
+            </button>
+            <button mat-raised-button color="primary" mat-dialog-close>Close</button>
+          </span>
+        </div>
+
+        <div class="pdf-viewer">
+          <ngx-extended-pdf-viewer
+            [customToolbar]="additionalButtons"
+            [src]="pdfSrc"
+            [textLayer]="true">
+          </ngx-extended-pdf-viewer>
+        </div>
+
+        <ng-template #additionalButtons>
+          <div id="toolbarViewer">
+            <pdf-zoom-toolbar ></pdf-zoom-toolbar>
+            <div id="toolbarViewerRight">
+              <pdf-presentation-mode></pdf-presentation-mode>
+              <pdf-print></pdf-print>
+              <div class="verticalToolbarSeparator hiddenSmallView"></div>
+              <pdf-toggle-secondary-toolbar></pdf-toggle-secondary-toolbar>
+            </div>
+          </div>
+        </ng-template>
       </div>
 
       <div *ngIf="pdfSrc === ''">
@@ -42,6 +74,12 @@ import { MatButtonModule } from '@angular/material/button';
       justify-content: center;
     }
 
+    .pdf-viewer{
+      padding: 2em;
+      height: 85%;
+      margin: 0 auto;
+    }
+
     .emptymsg div{
       margin: 0 auto;
     }
@@ -57,10 +95,48 @@ import { MatButtonModule } from '@angular/material/button';
       margin: 1em;
     }
 
+    .title .text{
+      font-size: 1.5em;
+      margin-top: 7px;
+    }
+
+    .title .icon{
+      margin-right: 0.5em;
+      margin-left: 0.5em;
+      color: #008080;
+      font-size: 1.7em;
+    }
+
     .empty-img{
       width: 25em;
       height: 25em;
       margin: 0 auto;
+    }
+
+    .pdf-viewer-container{
+      width: 100%;
+      height: 100%;
+    }
+
+    .title{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+
+    .action-buttons{
+      width: 15em;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+
+    .pdf-header{
+      width: 95%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      padding-top: 2em;
     }
   `,
   standalone: true,
@@ -69,7 +145,7 @@ class PdfViewerDialog {
   pdfSrc: string = '';
   title: string = ''; 
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private snackBar: MatSnackBar) {
     if (data.src == undefined || data.title == undefined) {
       this.pdfSrc = '';
       this.title = '';
@@ -91,11 +167,11 @@ class PdfViewerDialog {
   styles: ``
 })
 
-export class PdfViewerComponent implements OnInit{
+export class PdfViewerComponent{
   @Input() pdfSrc: string | undefined;
   @Input() title: string | undefined; 
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private snackBar: MatSnackBar) {}
 
   openDialog() {
     this.dialog.open(PdfViewerDialog, {
@@ -104,9 +180,5 @@ export class PdfViewerComponent implements OnInit{
         src: this.pdfSrc
       },
     });
-  }
-
-  ngOnInit(): void {
-    this.pdfSrc = 'C:/Users/user/OneDrive/Mora IT/Academics/4th Semester/CM2111 - Statistical Inference/Hypothesis Testing.pdf'
   }
 }
