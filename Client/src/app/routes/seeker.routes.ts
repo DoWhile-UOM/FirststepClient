227f8adb68eee7d7eff45364817595fc8013e1ff
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2024-04-28 16:24:41 +0530

    Remove unwanted components

--- Client/src/app/routes/seeker.routes.ts
+++ Client/src/app/routes/seeker.routes.ts
@@ -4,12 +4,11 @@ import { AdvertisementViewPageComponent } from '../components/advertisement-view
 import { SeekerHomePageComponent } from '../components/seeker-home-page/seeker-home-page.component';
 import { SavedAdvertisementListComponent } from '../components/saved-advertisement-list/saved-advertisement-list.component';
 import { CompanyProfileComponent } from '../components/company-profile/company-profile.component';
+
+// need to be verified
 import { SeekerApplicationFormComponent } from '../components/seeker-application-form/seeker-application-form.component';
 import { SeekerApplicationFormConfirmComponent } from '../components/seeker-application-form-confirm/seeker-application-form-confirm.component';
 import { SeekerApplicationStatusComponent } from '../components/seeker-application-status/seeker-application-status.component';
-import { SeekerSubmitedApplicationsComponent } from '../components/seeker-submited-applications/seeker-submited-applications.component';
-import { AdvertisementCardComponent } from '../components/advertisement-card/advertisement-card.component';
-import { AdvertisementCardReviewComponent } from '../components/advertisement-card-review/advertisement-card-review.component';
 
 
 export const routes: Routes = [
@@ -18,10 +17,10 @@ export const routes: Routes = [
   { path: 'home', component: SeekerHomePageComponent },
   { path: 'saved', component: SavedAdvertisementListComponent },
   { path: 'home/company-profile', component: CompanyProfileComponent },
+
+  // need to be verified
   { path: 'home/applicationForm', component: SeekerApplicationFormComponent },
   { path: 'home/applicationForm/applicationFormconfirm', component:SeekerApplicationFormConfirmComponent  },
-  { path: 'home/submitedApplications', component:SeekerSubmitedApplicationsComponent},
   { path: 'home/applicationReview', component:SeekerApplicationStatusComponent },
-  {path: 'advertismentcardReview',component:AdvertisementCardReviewComponent}
   
 ];
