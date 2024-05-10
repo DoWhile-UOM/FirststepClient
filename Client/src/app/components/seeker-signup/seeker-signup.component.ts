52c98c6c839943405759faa17d4957ed341fdb31
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 20:27:41 +0530

    seeker signup created

---
+++ Client/src/app/components/seeker-signup/seeker-signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seeker-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './seeker-signup.component.html',
+  styleUrl: './seeker-signup.component.css'
+})
+export class SeekerSignupComponent {
+
+}
