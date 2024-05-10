2e18c3de5bb71f8fd9f2bdf707b337700758d595
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2024-05-10 19:22:39 +0530

    Revert "Merge branch 'ruwandie-dev' into dev"
    This reverts commit 2ac44d9dec494647deab7f595c9d9b2675f4e841, reversing
    changes made to 0416d33f506287712a185a7859ec2f75487fb605.

--- Client/src/services/seeker.service.ts
+++ Client/src/services/seeker.service.ts
@@ -6,66 +6,31 @@ import axios from 'axios';
 })
 export class SeekerService {
 
-  async getSeekerDetails(id : number) {
-    let seekerData: any;
-    
-    await axios.get(Apipaths.getSeekerDetails  + id)
-      .then((response) => {
-        seekerData = response.data;
-      })
-      .catch (function (error) {
-        alert("Network Error: " + error);
-      });;
+async getSeekerDetails(id : number) {
+  let seekerData: any;
+  
+  await axios.get(Apipaths.getSeekerDetails  + id)
+    .then((response) => {
+      seekerData = response.data;
+    })
+    .catch (function (error) {
+      alert("Network Error: " + error);
+    });;
 
-    return seekerData;
-  }
+  return seekerData;
+}
 
-  async getSeekerDetailsForApplication(id : number) {
-    let seekerData: any;
-    
-    await axios.get(Apipaths.getSeekerDetailsForApplication  + id)
-      .then((response) => {
-        seekerData = response.data;
-      })
-      .catch (function (error) {
-        alert("Network Error: " + error);
-      });;
+async getSeekerDetailsForApplication(id : number) {
+  let seekerData: any;
+  
+  await axios.get(Apipaths.getSeekerDetailsForApplication  + id)
+    .then((response) => {
+      seekerData = response.data;
+    })
+    .catch (function (error) {
+      alert("Network Error: " + error);
+    });;
 
-    return seekerData;
-  }
-
-  //update method
-  async editseeker(seeker: any, seekerID: number) {
-    try {
-      await axios
-        .put('https://localhost:7213/api/Seeker/UpdateSeeker/' + seekerID, seeker)
-        .then((response) => {
-          console.log(response);
-        });
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  //delete method
-  async deleteseeker(seekerID: number) {
-    try {
-      await axios.delete('https://localhost:7213/api/Seeker/DeleteSeeker/1059' + seekerID);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  //add method
-  async addseeker(seeker: any) {
-    try {
-      seeker.seeker_id= 13; //sample company id
-      await axios.post('https://localhost:7213/api/Seeker/AddSeeker', seeker)
-      .then((response) => {
-        console.log(response);
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  return seekerData;
+}
 }
