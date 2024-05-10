7c1366e6ee45ee5711909ba2da282e2174e40f1f
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 20:30:59 +0530

    signup ts done

--- Client/src/app/components/seeker-signup/seeker-signup.component.ts
+++ Client/src/app/components/seeker-signup/seeker-signup.component.ts
@@ -57,5 +57,92 @@ interface newSeeker {
   styleUrl: './seeker-signup.component.css'
 })
 export class SeekerSignupComponent {
+  //file upload
+url = './assets/images/SeekerEdit.jpg';
+
+onselectFile(event: any) {
+  if (event.target.files && event.target.files.length > 0) {
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (e: any) => {
+      this.url = e.target.result;
+    };
+  }
+}
+
+seekerReg = this._formBuilder.group({
+  first_name: ['', Validators.required],
+  last_name: ['', Validators.required],
+  email: ['', Validators.required],
+  password_hash: ['', Validators.required],
+  phone_number: ['', Validators.required],
+  university: [''],
+  linkedin: [''],
+  field_id: ['', Validators.required],
+  cVurl: ['', Validators.required],
+  bio: ['', Validators.required],
+  otp: [''],
+  description: ['', Validators.required],
+  profile_picture: [''],
+  seekerSkills: this._formBuilder.array([]),
+});
+
+constructor(
+  private _formBuilder: FormBuilder,
+  private jobFieldService: JobfieldService,
+  private seekerService: SeekerService,
+  private _snackBar: MatSnackBar
+) {}
+
+fields: field[] = [];
+
+
+seekerDetails: newSeeker[] = [];
+
+async ngOnInit() {
+  await this.jobFieldService.getAll().then((response) => {
+    this.fields = response;
+    console.log(this.fields);
+  });
+}
+ngAfterViewInit() {
+	this.skills = this.addSkillsComponent.skills;
+}
+
+
+//skills
+skills: string[] = [];
+@ViewChild(AddSkillsComponent) addSkillsComponent!: AddSkillsComponent;
+changeSkillsArray($event: Event){
+	var skills = $event;
+	if (skills != null){
+		this.skills = skills as unknown as string[];
+	}
+	alert("Skills: " + this.skills);
+}
+
+//Submit button
+onRegister() {
+    // this.seekerReg.value.seekerSkills = this.skills.join(','); // Fix: Convert array of strings to a single string separated by commas
+    this.seekerService.SeekerRegister(this.seekerReg.value).subscribe({
+      next: (res) => {
+        this._snackBar.open("Registration successful", "Close", { duration: 3000 });
+      },
+      error: (err) => {
+        this.displayError(err);
+      }
+    });
+  
+}
+
+displayError(error: HttpErrorResponse) {
+  let message = 'An error occurred';
+  if (error.error instanceof ErrorEvent) {
+    message = `Client/network error: ${error.error.message}`;
+  } else {
+    message = `Server returned code ${error.status}, error message is: ${error.statusText}; Details: ${error.error}`;
+  }
+  this._snackBar.open(message, "Close", { duration: 5000 });
+}
 
 }
