ac9c9c6251ab90a9052811fa9e5a898eac250422
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 20:40:55 +0530

    s profileview ts done

--- Client/src/app/components/seeker-profile-view/seeker-profile-view.component.ts
+++ Client/src/app/components/seeker-profile-view/seeker-profile-view.component.ts
@@ -26,5 +26,22 @@ interface Seeker {
   styleUrl: './seeker-profile-view.component.css'
 })
 export class SeekerProfileViewComponent {
+  seekerDetails: Seeker = {} as Seeker;
 
+constructor(private seekerService: SeekerService) {}
+user_id: number = 2095;
+
+async ngOnInit() {
+  this.fetchSeekerDetails();
+}
+
+//get
+async fetchSeekerDetails() {
+  try {
+    const response = await this.seekerService.getSeekerDetails(this.user_id);
+    this.seekerDetails = response;
+  } catch (error) {
+    console.error('Error fetching seeker details:', error);
+  }
+}
 }
