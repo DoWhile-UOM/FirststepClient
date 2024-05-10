df966cbb21b185e851bb476264f8d1b5f8acf78d
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 20:34:42 +0530

    seeker profile view created

---
+++ Client/src/app/components/seeker-profile-view/seeker-profile-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seeker-profile-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './seeker-profile-view.component.html',
+  styleUrl: './seeker-profile-view.component.css'
+})
+export class SeekerProfileViewComponent {
+
+}
