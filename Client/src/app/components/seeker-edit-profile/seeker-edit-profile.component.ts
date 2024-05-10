2fc53325d63f19218735723f105a966453a1b422
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 20:16:19 +0530

    seeker edit ts done

--- Client/src/app/components/seeker-edit-profile/seeker-edit-profile.component.ts
+++ Client/src/app/components/seeker-edit-profile/seeker-edit-profile.component.ts
@@ -73,5 +73,100 @@ interface updateSeeker {
   styleUrl: './seeker-edit-profile.component.css'
 })
 export class SeekerEditProfileComponent {
+   // The image url of the default image
+ url = './assets/images/SeekerEdit.jpg';
+
+ //image upload
+ onselectFile(event: any) {
+   if (event.target.files) {
+     var reader = new FileReader();
+     reader.readAsDataURL(event.target.files[0]);
+     reader.onload = (event: any) => {
+       this.url = event.target.result;
+     };
+   }
+ }
+
+ seekerDetails: Seeker = {} as Seeker;
+ seekerUpdate: updateSeeker = {} as updateSeeker;
+
+ constructor(
+   private seekerService: SeekerService,
+   private jobFieldService: JobfieldService
+ ) {}
+ user_id: number = 2095;
+
+ fields: job_Field[] = [];
+
+ selectedFieldId!: number;
+
+ async ngOnInit() {
+   await this.fetchSeekerDetails();
+   await this.jobFieldService.getAll().then((response) => {
+     this.fields = response;
+     this.selectedFieldId = this.seekerDetails.field_id; // Assuming this is how you fetch the current field ID
+   });
+ }
+
+ async ngAfterViewInit() {
+   this.skills = this.addSkillsComponent.skills;
+ }
+ data(data: any) {
+   throw new Error('Method not implemented.');
+ }
+
+ //radiobutton
+ selected = false;
+
+ onRadioChange() {
+   this.selected = true;
+ }
+
+ //get
+ async fetchSeekerDetails() {
+   try {
+     const response = await this.seekerService.getSeekerDetails(this.user_id);
+     this.seekerDetails = response;
+   } catch (error) {
+     console.error('Error fetching seeker details:', error);
+   }
+ }
+
+ //skills
+
+ skills: string[] = [];
+ @ViewChild(AddSkillsComponent) addSkillsComponent!: AddSkillsComponent;
+
+ changeSkillsArray($event: Event) {
+   var skills = $event;
+   if (skills != null) {
+     this.skills = skills as unknown as string[];
+   }
+   alert('Skills: ' + this.skills);
+ }
+
+ //update
+//  async onApply() {
+//    //this.seekerUpdate.field_id = this.selectedFieldId; //fetch the selected field id
+//    // this.seekerUpdate.seekerSkills = this.skills;
+//    // this.seekerUpdate.seekerSkills = this.skills;
+//    try {
+//      await this.seekerService.editseeker(this.seekerUpdate, this.user_id);
+//    } catch (error) {
+//      console.error('error updating profile', error);
+//    }
+//  }
+
+//  //Delete
+
+//  async onDelete(user_id: number) {
+//    try {
+//      await this.seekerService.deleteseeker(this.user_id).then(() => {
+//        this.seekerDetails = {} as Seeker;
+//      });
+//    } catch (error) {
+//      console.error('Error deleting seeker:', error);
+//    }
+//  }
 
 }
