b942d767e8691add4350ad98ba41f25defa19e27
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 21:17:57 +0530

    HRMApplicationView created

---
+++ Client/src/app/components/hrmanager-application-view/hrmanager-application-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hrmanager-application-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './hrmanager-application-view.component.html',
+  styleUrl: './hrmanager-application-view.component.css'
+})
+export class HrmanagerApplicationViewComponent {
+
+}
