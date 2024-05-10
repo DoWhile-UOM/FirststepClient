f2109dbcaa804e1a70eaea72c6dd71554afd709b
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 20:25:53 +0530

    seeker route updated

--- Client/src/app/routes/seeker.routes.ts
+++ Client/src/app/routes/seeker.routes.ts
@@ -5,6 +5,7 @@ import { SeekerHomePageComponent } from '../components/seeker-home-page/seeker-h
 import { SavedAdvertisementListComponent } from '../components/saved-advertisement-list/saved-advertisement-list.component';
 import { ApplicationAdvertisementListComponent } from '../components/application-advertisement-list/application-advertisement-list.component';
 import { CompanyProfileComponent } from '../components/company-profile/company-profile.component';
+import { SeekerEditProfileComponent } from '../components/seeker-edit-profile/seeker-edit-profile.component';
 
 // need to be verified
 import { SeekerApplicationFormComponent } from '../components/seeker-application-form/seeker-application-form.component';
@@ -19,6 +20,7 @@ export const routes: Routes = [
   { path: 'saved', component: SavedAdvertisementListComponent },
   { path: 'applied', component: ApplicationAdvertisementListComponent },
   { path: 'home/company-profile', component: CompanyProfileComponent },
+  { path: 'home/edit-profile', component: SeekerEditProfileComponent },
 
   // need to be verified
   { path: 'home/applicationForm', component: SeekerApplicationFormComponent },
