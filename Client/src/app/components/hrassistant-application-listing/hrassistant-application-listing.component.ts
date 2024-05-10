25c3dc5a179306678dc36e86fb8f58fd0be261ad
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 21:04:11 +0530

    HRA component created

---
+++ Client/src/app/components/hrassistant-application-listing/hrassistant-application-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hrassistant-application-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './hrassistant-application-listing.component.html',
+  styleUrl: './hrassistant-application-listing.component.css'
+})
+export class HrassistantApplicationListingComponent {
+
+}
