436546b262c76ca2f7af66bfed0623eb6b6b479d
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 20:43:24 +0530

    HRM applist created

---
+++ Client/src/app/components/hr-manager-application-listing/hr-manager-application-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hr-manager-application-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './hr-manager-application-listing.component.html',
+  styleUrl: './hr-manager-application-listing.component.css'
+})
+export class HrManagerApplicationListingComponent {
+
+}
