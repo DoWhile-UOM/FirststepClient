ebc6350d8f6ee9986498be877abf52b8ab2fa75d
RuwandieD <dharmapalard.21@uom.lk>
2024-05-10 19:57:32 +0530

    seeker edit profile component created

---
+++ Client/src/app/components/seeker-edit-profile/seeker-edit-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seeker-edit-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './seeker-edit-profile.component.html',
+  styleUrl: './seeker-edit-profile.component.css'
+})
+export class SeekerEditProfileComponent {
+
+}
