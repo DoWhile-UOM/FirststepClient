f95167cc9ec00b0dd7e60debac9e3a216aceef55
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-05-27 16:51:24 +0530

    delete comments

--- Client/src/services/application.service.ts
+++ Client/src/services/application.service.ts
@@ -2,60 +2,26 @@ import { Injectable } from '@angular/core';
 import axios from 'axios';
 import { Apipaths } from './apipaths/apipaths';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApplicationService {
-
-  constructor() { }
+  constructor() {}
 
   async submitSeekerApplication(applicationData: FormData): Promise<void> {
-   try{
-    const response = await axios.post(Apipaths.submitApplication, applicationData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    });
-    console.log(response.data);
-   }
-    catch (error) {
+    try {
+      const response = await axios.post(
+        Apipaths.submitApplication,
+        applicationData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
       console.error('Error submiting application: ', error);
+    }
   }
 }
-}
- /* async submitSeekerApplication(applications: any) {
-    try{
-      await axios.post(Apipaths.submitApplication, applications)
-        .then((response) => {
-          console.log(response);
-        });
-    }
-    catch (error) {
-      console.error(error);
-    }
-  }*///comment by nethma
-
-  // async getAllApplicationsbyAdvertisementID(job_id: number) {
-  //   let applicationList: any = [];
-  
-  //   await axios.get(Apipaths.getAllApplications + job_id)
-  //     .then(function (response) {
-  //       try {
-  //         applicationList = response.data;
-  //       }
-  //       catch (error) {
-  //         console.log("No applications found");
-  //       }
-  //     })
-  //     .catch(
-  //       function (error) {
-  //         alert('Network Error: ' + error);
-  //       }
-  //     );
-  
-  //   return applicationList;
-
-
-
