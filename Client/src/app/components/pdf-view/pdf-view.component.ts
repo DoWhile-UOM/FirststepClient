baabff86ef66bb487d944c6a8acf05c1ccabe1c5
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-05-29 16:57:07 +0530

    pdf viewer view pdf using an url

--- Client/src/app/components/pdf-view/pdf-view.component.ts
+++ Client/src/app/components/pdf-view/pdf-view.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { PdfViewerModule } from 'ng2-pdf-viewer'; // Import the PdfViewerComponent and PdfViewerModule
+import { PdfViewerModule } from '@syncfusion/ej2-angular-pdfviewer';
 
 @Component({
   selector: 'app-pdf-view',
   standalone: true,
-  imports: [PdfViewerModule],
+  imports: [PdfViewerModule], 
   templateUrl: './pdf-view.component.html',
   styleUrl: './pdf-view.component.css'
 })
-export class PdfViewComponent {
-
-  pdfSrc = "src/assets/cv.pdf";
-
+export class PdfViewComponent { 
+  public document: string = 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
+  public resource: string = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
 }
