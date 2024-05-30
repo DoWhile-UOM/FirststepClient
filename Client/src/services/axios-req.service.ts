914954451867a3468452ca7f2c3edf39d71c1d7d
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-05-30 21:46:49 +0530

    axios inceptor service complete

--- Client/src/services/axios-req.service.ts
+++ Client/src/services/axios-req.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import axios, { AxiosInstance } from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AxiosReqService {
+  private axiosInstance: AxiosInstance;
 
-  constructor() { }
+  constructor() {
+    this.axiosInstance = axios.create();
+
+    // Set up the axios interceptor
+    this.axiosInstance.interceptors.request.use(
+      config => {
+        const authToken = sessionStorage.getItem('token');
+        if (authToken) {
+          config.headers.Authorization = `Bearer ${authToken}`;
+        }
+        return config;
+      },
+      error => {
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  getAxiosInstance(): AxiosInstance {
+    return this.axiosInstance;
+  }
 }
