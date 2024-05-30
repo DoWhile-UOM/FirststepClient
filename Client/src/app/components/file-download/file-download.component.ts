e2ec779882c6613f6756e663c22fdc1d680e6e31
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-05-30 20:24:47 +0530

    pdf as a popup

--- Client/src/app/components/file-download/file-download.component.ts
+++ Client/src/app/components/file-download/file-download.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { PdfViewComponent } from '../pdf-view/pdf-view.component';
+
 
 @Component({
     selector: 'app-file-download',
     standalone: true,
     templateUrl: './file-download.component.html',
     styleUrl: './file-download.component.css',
-    imports: [MatIconModule]
+    imports: [MatIconModule,MatButtonModule]
 })
 export class FileDownloadComponent {
 
+constructor(
+    public dialog: MatDialog
+) {} 
+
+openpdf() {
+    const dialogRef = this.dialog.open(PdfViewComponent);
+}
 }
--- Client/src/app/components/pdf-view/pdf-view.component.ts
+++ Client/src/app/components/pdf-view/pdf-view.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { PdfViewerModule } from '@syncfusion/ej2-angular-pdfviewer';
+import { MatDialogModule, MatDialogContent} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-pdf-view',
   standalone: true,
-  imports: [PdfViewerModule], 
+  imports: [PdfViewerModule,MatDialogModule, MatDialogContent,], 
   templateUrl: './pdf-view.component.html',
   styleUrl: './pdf-view.component.css'
 })
 export class PdfViewComponent { 
+
+  constructor(
+  ) { }
   public document: string = 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
   public resource: string = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
 }
