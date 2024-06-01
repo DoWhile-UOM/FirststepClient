8f94a19b6256a617fc1932a23f83e91c52229c28
RuwandieD <dharmapalard.21@uom.lk>
2024-06-01 23:00:29 +0530

    updated route for profile view and added evaluation status

--- Client/src/app/routes/hrm.routes.ts
+++ Client/src/app/routes/hrm.routes.ts
@@ -5,6 +5,7 @@ import { RoleProfileEditComponent } from '../components/role-profile-edit/role-p
 import { HrManagerApplicationListingComponent } from '../components/hr-manager-application-listing/hr-manager-application-listing.component';
 import { NewJobComponent, NewJobUploadedComponent } from '../components/new-job/new-job.component';
 import { HrmanagerApplicationViewComponent } from '../components/hrmanager-application-view/hrmanager-application-view.component';
+import { SeekerProfileViewComponent } from '../components/seeker-profile-view/seeker-profile-view.component';
 
 export const hrmRoutes: Routes = [
   { path: '', redirectTo: 'jobOfferList', pathMatch: 'full' },
@@ -16,4 +17,5 @@ export const hrmRoutes: Routes = [
   { path: 'jobOfferList/Uploaded', component: NewJobUploadedComponent},
   { path: 'jobOfferList/applicationList', component: HrManagerApplicationListingComponent },
   { path: 'jobOfferList/applicationList/applicationView', component: HrmanagerApplicationViewComponent },
+  { path: 'jobOfferList/applicationList/applicationView/seekerProfileView/:seekerId', component: SeekerProfileViewComponent }
 ];
