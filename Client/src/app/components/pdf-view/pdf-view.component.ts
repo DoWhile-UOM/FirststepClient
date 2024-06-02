f02a053d4380a82b098d0a754ccd083165b44528
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-06-02 22:13:06 +0530

    changes

--- Client/src/app/components/pdf-view/pdf-view.component.ts
+++ Client/src/app/components/pdf-view/pdf-view.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PdfViewerModule } from '@syncfusion/ej2-angular-pdfviewer';
 import { MatDialogModule, MatDialogContent} from '@angular/material/dialog';
+import { DocumentService } from '../../../services/document.service';
 
 @Component({
   selector: 'app-pdf-view',
   standalone: true,
-  imports: [PdfViewerModule,MatDialogModule, MatDialogContent,], 
+  imports: [PdfViewerModule, MatDialogModule, MatDialogContent,], 
   templateUrl: './pdf-view.component.html',
   styleUrl: './pdf-view.component.css'
 })
-export class PdfViewComponent { 
-
-  constructor(
-  ) { }
-  public document: string = 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf';
+export class PdfViewComponent implements OnInit{ 
+ 
+  documentName: string = 'Indexes.pdf';
+  constructor(private documentService:DocumentService) { }
+  public document: string = "" ;
   public resource: string = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
+
+  ngOnInit(): void {
+  this.documentService.generateSasToken(this.documentName).subscribe(
+    (token:string) => {
+      this.document= this.documentService.getBlobUrl(this.documentName, token);
+      
+    },
+    error => {
+      console.error('Error fetching SAS token:', error);
+    }
+  );
+}
+
+
+
 }
