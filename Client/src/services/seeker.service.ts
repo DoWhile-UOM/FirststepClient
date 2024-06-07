8485c90742de51ba1cf11bfc0fd4e7f549bc660a
RuwandieD <dharmapalard.21@uom.lk>
2024-06-07 23:39:06 +0530

    Revert "A change in seeker service"
    This reverts commit 6fa8ace6236c96d3f6e867b08fb58d14da964fbe.

--- Client/src/services/seeker.service.ts
+++ Client/src/services/seeker.service.ts
@@ -1,82 +1,83 @@
 import { Injectable } from '@angular/core';
+import { Apipaths } from './apipaths/apipaths';
 import axios from 'axios';
 import { HttpClient } from '@angular/common/http';
-import * as https from 'https';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SeekerService {
-  private axiosInstance = axios.create({
-    baseURL: 'https://localhost:7213/api',
-    httpsAgent: new https.Agent({  
-      rejectUnauthorized: false
+  
+constructor(private http: HttpClient) { }
+
+async getSeekerDetails(id : number) {
+  let seekerData: any;
+  
+  await axios.get('https://localhost:7213/api/Seeker/GetSeeker/' + id)
+    .then((response) => {
+      seekerData = response.data;
     })
-  });
+    .catch (function (error) {
+      console.log("Network Error in getSeekerDetails : " + error);
+    });;
 
-  constructor(private http: HttpClient) { }
+  return seekerData;
+}
 
-  async getSeekerDetails(id: number) {
-    let seekerData: any;
+async getSeekerDetailsForApplication(id : number) {
+  let seekerData: any;
+  
+  await axios.get(Apipaths.getSeekerDetailsForApplication  + id)
+    .then((response) => {
+      seekerData = response.data;
+    })
+    .catch (function (error) {
+      console.log("Network Error: " + error);
+    });;
 
-    await this.axiosInstance.get(`/Seeker/GetSeeker/${id}`)
+  return seekerData;
+}
+
+//update method
+async editseeker(seeker: any, seekerID: number) {
+  try {
+    await axios
+      .put('https://localhost:7213/api/Seeker/UpdateSeeker/' + seekerID, seeker)
       .then((response) => {
-        seekerData = response.data;
-      })
-      .catch(function (error) {
-        console.log("Network Error in getSeekerDetails : " + error);
-      });
-
-    return seekerData;
+        console.log(response);
+      });console.log("seeker updated successfully");
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  async getSeekerDetailsForApplication(id: number) {
-    let seekerData: any;
-
-    await this.axiosInstance.get(`/Seeker/GetSeekerDetailsForApplication/${id}`)
-      .then((response) => {
-        seekerData = response.data;
-      })
-      .catch(function (error) {
-        console.log("Network Error: " + error);
-      });
-
-    return seekerData;
+//delete method
+async deleteseeker(seekerID: number) {
+  try {
+    await axios.delete('https://localhost:7213/api/Seeker/DeleteSeeker/' + seekerID);
+    console.log("seeker deleted successfully");
+  } catch (error) {
+    ///console.error(error);
   }
+}
 
-  async editSeeker(seeker: any, seekerID: number) {
-    try {
-      await this.axiosInstance.put(`/Seeker/UpdateSeeker/${seekerID}`, seeker)
-        .then((response) => {
-          console.log(response);
-        });
-      console.log("Seeker updated successfully");
-    } catch (error) {
-      console.error(error);
-    }
-  }
+//register method
+SeekerRegister(seeker:any){
+  return this.http.post<any>('https://localhost:7213/api/Seeker/AddSeeker',seeker)
+}
 
-  async deleteSeeker(seekerID: number) {
-    try {
-      await this.axiosInstance.delete(`/Seeker/DeleteSeeker/${seekerID}`);
-      console.log("Seeker deleted successfully");
-    } catch (error) {
-      console.error(error);
-    }
+//add method
+async addseeker(seeker: any) {
+  try {
+    await axios.post('https://localhost:7213/api/Seeker/AddSeeker', seeker)
+     .then((response) => {
+       console.log(response);
+     });
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  SeekerRegister(seeker: any) {
-    return this.http.post<any>('https://localhost:7213/api/Seeker/AddSeeker', seeker);
-  }
-
-  async addSeeker(seeker: any) {
-    try {
-      await this.axiosInstance.post('/Seeker/AddSeeker', seeker)
-        .then((response) => {
-          console.log(response);
-        });
-    } catch (error) {
-      console.error(error);
-    }
-  }
 }
