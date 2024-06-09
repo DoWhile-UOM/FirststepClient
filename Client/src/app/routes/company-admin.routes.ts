e64e88fb43e4d64b7703e602629277c7bbfac2af
RuwandieD <dharmapalard.21@uom.lk>
2024-06-09 16:33:40 +0530

    ca route updated

--- Client/src/app/routes/company-admin.routes.ts
+++ Client/src/app/routes/company-admin.routes.ts
@@ -7,6 +7,7 @@ import { CompanyProfileEditComponent } from '../components/company-profile-edit/
 import { RoleProfileEditComponent } from '../components/role-profile-edit/role-profile-edit.component';
 import { HrManagerApplicationListingComponent } from '../components/hr-manager-application-listing/hr-manager-application-listing.component';
 import { HrmanagerApplicationViewComponent } from '../components/hrmanager-application-view/hrmanager-application-view.component';
+import { RegisterCompanyComponent } from '../components/register-company/register-company.component';
 
 export const caRoutes: Routes = [
   { path: '', redirectTo: 'jobOfferList', pathMatch: 'full' },
@@ -19,4 +20,5 @@ export const caRoutes: Routes = [
   { path: 'jobOfferList/applicationList/applicationView', component: HrmanagerApplicationViewComponent },
   { path: 'companyProfile', component: CompanyProfileEditComponent },
   { path: 'editRoleProfile', component: RoleProfileEditComponent },
+  { path: 'registerCompany', component: RegisterCompanyComponent },
 ];
