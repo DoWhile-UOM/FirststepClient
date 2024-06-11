21a3eac5e79eb2c60786c18715d54253e3dc7916
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-06-11 11:35:20 +0530

    back button

--- Client/src/app/components/pdf-view/pdf-view.component.ts
+++ Client/src/app/components/pdf-view/pdf-view.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { PdfViewerModule } from '@syncfusion/ej2-angular-pdfviewer';
-import { MatDialogModule, MatDialogContent, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { DocumentService } from '../../../services/document.service';
+import { MatDialogModule, MatDialogContent, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import { DialogData } from '../company-application/company-application.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -13,18 +12,22 @@ import {MatButtonModule} from '@angular/material/button';
   templateUrl: './pdf-view.component.html',
   styleUrl: './pdf-view.component.css'
 })
-export class PdfViewComponent implements OnInit{ 
- 
+export class PdfViewComponent implements OnInit{
 
   public document: any;
   public resource: string = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: DialogData)
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private dialogRef: MatDialogRef<PdfViewComponent>)
   { }
   
   ngOnInit() {
     this.document = this.data.documentUrl;
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
   }
 
 }
