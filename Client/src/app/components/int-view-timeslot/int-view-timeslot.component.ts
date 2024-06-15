d3b17dd38fcc72b3f733f93c3252460d434b1daa
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-06-16 01:14:34 +0530

    Timeslot given Intview

---
+++ Client/src/app/components/int-view-timeslot/int-view-timeslot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-int-view-timeslot',
+  standalone: true,
+  imports: [],
+  templateUrl: './int-view-timeslot.component.html',
+  styleUrl: './int-view-timeslot.component.css'
+})
+export class IntViewTimeslotComponent {
+
+}
