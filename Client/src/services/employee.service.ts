66cea935337880ad3820afcbdc244c7501955553
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-06-15 09:14:42 +0530

    add snack bars

--- Client/src/services/employee.service.ts
+++ Client/src/services/employee.service.ts
@@ -95,10 +95,13 @@ export class EmployeeService {
       await axios.post(Apipaths.addNewHRManager, employee)
         .then((response) => {
           console.log(response);
+          this.snackBar.open('HR Manager added successfully', "", { panelClass: ['app-notification-normal'] })._dismissAfter(3000);
+
         });
     }
     catch (error) {
       console.error(error);
+      this.snackBar.open('Error adding HR Manager', "", { panelClass: ['app-notification-error'] })._dismissAfter(3000);
     }
   }
 
@@ -107,10 +110,12 @@ export class EmployeeService {
       await axios.post(Apipaths.addNewHRAssistant, employee)
         .then((response) => {
           console.log(response);
+          this.snackBar.open('HR Assistant added successfully', "", { panelClass: ['app-notification-normal'] })._dismissAfter(3000);
         });
     }
     catch (error) {
       console.error(error);
+      this.snackBar.open('Error adding HR Assistant', "", { panelClass: ['app-notification-error'] })._dismissAfter(3000);
     }
   }
 
@@ -119,17 +124,23 @@ export class EmployeeService {
       await axios.put(Apipaths.editemployee + employeeID, employee)
         .then((response) => {
           console.log(response);
+          this.snackBar.open('Employee details updated successfully', "", { panelClass: ['app-notification-normal'] })._dismissAfter(3000);
         });
 
     }
     catch (error) {
       console.error(error);
+      this.snackBar.open('Error updating employee details', "", { panelClass: ['app-notification-error'] })._dismissAfter(3000);
     }
   }
 
   async deleteEmployee(employeeID: number) {
     try {
       await axios.delete(Apipaths.deleteEmployee + employeeID)
+        .then((response) => {
+          console.log(response);
+          this.snackBar.open('Employee deleted successfully', "", { panelClass: ['app-notification-normal'] })._dismissAfter(3000);
+        });
     }
     catch (error) {
       console.error(error);
