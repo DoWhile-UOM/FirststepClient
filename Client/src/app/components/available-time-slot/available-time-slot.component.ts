b9cddf4fc69d9268f74f2f0c9b03f19ee936acaa
RuwandieD <dharmapalard.21@uom.lk>
2024-06-16 22:34:18 +0530

    time slots available but cannot be clicked on date

--- Client/src/app/components/available-time-slot/available-time-slot.component.ts
+++ Client/src/app/components/available-time-slot/available-time-slot.component.ts
@@ -1,18 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatTable } from '@angular/material/table';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { FormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common'; // Import DatePipe
-
 
 interface TimeSlot {
   date: string;
@@ -24,48 +21,59 @@ interface TimeSlot {
 @Component({
   selector: 'app-available-time-slot',
   standalone: true,
-  imports: [MatTable, MatTableModule, MatButtonModule, MatSnackBarModule, MatInputModule, MatFormFieldModule, MatIconModule, CommonModule, MatGridListModule, MatTabsModule, FormsModule],
-  providers: [DatePipe], // Add DatePipe to providers
+  imports: [
+    MatTableModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    CommonModule,
+    MatGridListModule,
+    MatTabsModule,
+    FormsModule
+  ],
+  providers: [DatePipe],
   templateUrl: './available-time-slot.component.html',
-  styleUrl: './available-time-slot.component.css'
+  styleUrls: ['./available-time-slot.component.css']
 })
-export class AvailableTimeSlotComponent  implements OnInit {
+export class AvailableTimeSlotComponent implements OnInit {
   timeSlots: { [key: string]: TimeSlot[] } = {
     '2024-05-31': [
-      { date: '2024-05-31', day: 'Monday', time: '6 AM - 9 AM', duration: '45 min' }
+      { date: '2024-05-31', day: 'Friday', time: '6 AM - 9 AM', duration: '45 min' }
     ],
     '2024-06-01': [
-      { date: '2024-06-01', day: 'Tuesday', time: '9 AM - 10 AM', duration: '60 min' },
-      { date: '2024-06-01', day: 'Tuesday', time: '10 AM - 11 AM', duration: '45 min' }
+      { date: '2024-06-01', day: 'Saturday', time: '9 AM - 10 AM', duration: '60 min' },
+      { date: '2024-06-01', day: 'Saturday', time: '10 AM - 11 AM', duration: '45 min' }
     ],
     '2024-06-02': [
-      { date: '2024-06-02', day: 'Wednesday', time: '10 AM - 11 AM', duration: '45 min' },
-      { date: '2024-06-02', day: 'Wednesday', time: '11 AM - 12 PM', duration: '30 min' }
+      { date: '2024-06-02', day: 'Sunday', time: '10 AM - 11 AM', duration: '45 min' },
+      { date: '2024-06-02', day: 'Sunday', time: '11 AM - 12 PM', duration: '30 min' }
     ],
     '2024-06-03': [
-      { date: '2024-06-03', day: 'Thursday', time: '11 AM - 12 PM', duration: '30 min' }
+      { date: '2024-06-03', day: 'Monday', time: '11 AM - 12 PM', duration: '30 min' }
     ],
     '2024-06-04': [
-      { date: '2024-06-04', day: 'Friday', time: '1 PM - 2 PM', duration: '60 min' }
+      { date: '2024-06-04', day: 'Tuesday', time: '1 PM - 2 PM', duration: '60 min' }
     ],
     '2024-06-05': [
-      { date: '2024-06-05', day: 'Saturday', time: '2 PM - 3 PM', duration: '45 min' }
+      { date: '2024-06-05', day: 'Wednesday', time: '2 PM - 3 PM', duration: '45 min' }
     ],
     '2024-06-06': [
-      { date: '2024-06-06', day: 'Sunday', time: '3 PM - 4 PM', duration: '45 min' }
+      { date: '2024-06-06', day: 'Thursday', time: '3 PM - 4 PM', duration: '45 min' }
     ],
     '2024-06-07': [
-      { date: '2024-06-07', day: 'Monday', time: '4 PM - 5 PM', duration: '30 min' }
+      { date: '2024-06-07', day: 'Friday', time: '4 PM - 5 PM', duration: '30 min' }
     ],
     '2024-06-08': [
-      { date: '2024-06-08', day: 'Tuesday', time: '5 PM - 6 PM', duration: '60 min' }
+      { date: '2024-06-08', day: 'Saturday', time: '5 PM - 6 PM', duration: '60 min' }
     ],
     '2024-06-09': [
-      { date: '2024-06-09', day: 'Wednesday', time: '6 PM - 7 PM', duration: '45 min' }
+      { date: '2024-06-09', day: 'Sunday', time: '6 PM - 7 PM', duration: '45 min' }
     ]
   };
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(private snackBar: MatSnackBar, public datePipe: DatePipe) {}
 
   ngOnInit(): void {}
 
@@ -78,7 +86,7 @@ export class AvailableTimeSlotComponent  implements OnInit {
   }
 
   editTimeSlot(date: string, index: number, time: string, duration: string) {
-    this.timeSlots[date][index] = { ...this.timeSlots[date][index], time, duration }; // Pass correct date
+    this.timeSlots[date][index] = { ...this.timeSlots[date][index], time, duration };
     this.snackBar.open('Time slot edited', 'Close', { duration: 3000 });
   }
 
@@ -91,6 +99,6 @@ export class AvailableTimeSlotComponent  implements OnInit {
   }
 
   getObjectKeys(obj: object): string[] {
-    return Object.keys(obj).sort((a, b) => new Date(a).getTime() - new Date(b).getTime()); // Sort keys
+    return Object.keys(obj).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
   }
 }
