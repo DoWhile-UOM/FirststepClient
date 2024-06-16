3c0c2eb9a992195d7e47116b40ff2ccdb498d1c2
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-06-16 10:54:46 +0530

    add styles

--- Client/src/app/components/interview-sheduling-short-list/interview-sheduling-short-list.component.ts
+++ Client/src/app/components/interview-sheduling-short-list/interview-sheduling-short-list.component.ts
@@ -4,6 +4,8 @@ import { MatTable,MatTableModule} from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from "../spinner/spinner.component";
+import { InterviewShedulingBackActionComponent } from "../interview-sheduling-back-action/interview-sheduling-back-action.component";
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 
@@ -22,11 +24,21 @@ export interface CandidateData{
     standalone: true,
     templateUrl: './interview-sheduling-short-list.component.html',
     styleUrl: './interview-sheduling-short-list.component.css',
-    imports: [MatButtonModule, MatCardModule, CommonModule, MatTableModule, SpinnerComponent]
+    imports: [MatButtonModule, MatCardModule, CommonModule, MatTableModule, SpinnerComponent, InterviewShedulingBackActionComponent,MatCheckboxModule]
 })
 export class InterviewShedulingShortListComponent {
   displayedColumns: string[] = ['position', 'name', 'lastRevisionBy', 'interview', 'application'];
-  candidateData: CandidateData[] = [];
+  candidateData: CandidateData[] = [
+      {id: 1, name: 'John Doe', lastRevisionBy: 'Jane Doe', interview: false,},
+      {id: 2, name: 'Jane Doe', lastRevisionBy: 'John Doe', interview: false,},
+      {id: 3, name: 'John Smith', lastRevisionBy: 'Jane Smith', interview: false},
+      {id: 4, name: 'Jane Smith', lastRevisionBy: 'John Smith', interview: false},
+      {id: 5, name: 'John Doe', lastRevisionBy: 'Jane Doe', interview: false},
+      {id: 6, name: 'Jane Doe', lastRevisionBy: 'John Doe', interview: false},
+      {id: 7, name: 'John Smith', lastRevisionBy: 'Jane Smith', interview: false},
+      {id: 8, name: 'Jane Smith', lastRevisionBy: 'John Smith', interview: false},
+      {id: 9, name: 'John Doe', lastRevisionBy: 'Jane Doe', interview: false},
+    ];
 
   company_id: string = ''; // sample company_id
   selected: string = 'all';
