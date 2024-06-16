0c366947900c2e7e9fd9513926744604f0aaf170
RuwandieD <dharmapalard.21@uom.lk>
2024-06-16 11:32:34 +0530

    daily interview schedules created

---
+++ Client/src/app/components/daily-interview-schedules/daily-interview-schedules.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-interview-schedules',
+  standalone: true,
+  imports: [],
+  templateUrl: './daily-interview-schedules.component.html',
+  styleUrl: './daily-interview-schedules.component.css'
+})
+export class DailyInterviewSchedulesComponent {
+
+}
