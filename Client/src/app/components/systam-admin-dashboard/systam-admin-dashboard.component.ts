db03f86c271e637e15a0e140233c57880822ca6c
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-06-20 02:38:12 +0530

    Feat(SA dashboard)-css changes

--- Client/src/app/components/systam-admin-dashboard/systam-admin-dashboard.component.ts
+++ Client/src/app/components/systam-admin-dashboard/systam-admin-dashboard.component.ts
@@ -12,6 +12,8 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ChangeDetectionStrategy, signal } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 interface EligibleUnregisteredCompany {
   company_id: number;
   company_name: string;
@@ -45,7 +47,8 @@ interface Logging {
     MatGridListModule,
     MatProgressBarModule,
     MatExpansionModule,
-    MatTabsModule
+    MatTabsModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './systam-admin-dashboard.component.html',
   styleUrls: ['./systam-admin-dashboard.component.css'],
@@ -70,6 +73,20 @@ export class SystamAdminDashboardComponent implements OnInit, AfterViewInit {
   items: EligibleUnregisteredCompany[] = [];
 
   loggings: Logging = {} as Logging;
+
+  active: number = 0;
+  inactive: number = 0;
+
+  percentageActiveCmpyUsers: number = 0;
+  percentageActiveSeekers: number = 0;
+  percentageActiveCA: number = 0;
+  percentageActiveHRM: number = 0;
+  percentageActiveHRA: number = 0;
+
+  subTab = 'active';
+  tab = 'ca';
+  catName = '';
+  percentage: number = 0;
 
   readonly panelOpenState = signal(false);
 
@@ -116,6 +133,11 @@ export class SystamAdminDashboardComponent implements OnInit, AfterViewInit {
     try {
       this.spinner.show();
       this.loggings = await this.userService.getLogging();
+      this.getPercentageActiveCmpyUsers();
+      this.getPercentageActiveSeekers();
+      this.getPercentageActiveCA();
+      this.getPercentageActiveHRM();
+      this.getPercentageActiveHRA();
     }
     finally {
       this.spinner.hide();
@@ -126,5 +148,85 @@ export class SystamAdminDashboardComponent implements OnInit, AfterViewInit {
     this.currentPage = event.pageIndex;
     this.pageSize = event.pageSize;
     this.items = this.eligibleUnregisteredCompanies.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize);
+  }
+  onTabChange(event: MatTabChangeEvent): void {
+    if (event.tab.textLabel === 'CA') {
+      this.active = this.loggings.activeCA;
+      this.inactive = this.loggings.inactiveCA;
+      this.tab = 'ca';
+    }
+    else if (event.tab.textLabel === 'HRM') {
+      this.active = this.loggings.activeHRM;
+      this.inactive = this.loggings.inactiveHRM;
+      this.tab = 'hrm';
+    }
+    else if (event.tab.textLabel === 'HRA') {
+      this.active = this.loggings.activeHRA;
+      this.inactive = this.loggings.inactiveHRA;
+      this.tab = 'hra';
+    }
+    else {
+      this.active = this.loggings.activeCA;
+      this.inactive = this.loggings.inactiveCA;
+      this.tab = 'ca';
+    }
+  }
+  onSubTabChange(event: MatTabChangeEvent): void {
+    if (event.tab.textLabel === 'Active') {
+      this.subTab = 'active';
+    }
+    else if (event.tab.textLabel === 'Inactive') {
+      this.subTab = 'inactive';
+    }
+    else {
+      this.subTab = 'active';
+    }
+    this.calculatePercentage();
+  }
+
+  calculatePercentage() {
+    if (this.subTab === 'active') {
+      if (this.tab === 'ca') {
+        this.percentage = this.percentageActiveCA;
+        this.catName = 'Active Company Admins';
+      }
+      else if (this.tab === 'hrm') {
+        this.percentage = this.percentageActiveHRM;
+        this.catName = 'Active HR Managers';
+      }
+      else if (this.tab === 'hra') {
+        this.percentage = this.percentageActiveHRA;
+        this.catName = 'Active HR Assistants';
+      }
+    }
+    else {
+      if (this.tab === 'ca') {
+        this.percentage = 100 - this.percentageActiveCA;
+        this.catName = 'Inactive Company Admins';
+      }
+      else if (this.tab === 'hrm') {
+        this.percentage = 100 - this.percentageActiveHRM;
+        this.catName = 'HR Managers';
+      }
+      else if (this.tab === 'hra') {
+        this.percentage = 100 - this.percentageActiveHRA;
+        this.catName = 'HR Assistants';
+      }
+    }
+  }
+  getPercentageActiveCmpyUsers() {
+    this.percentageActiveCmpyUsers = Number((this.loggings.activeCmpUsers / (this.loggings.activeCmpUsers + this.loggings.inactiveCmpUsers) * 100).toFixed(2));
+  }
+  getPercentageActiveSeekers() {
+    this.percentageActiveSeekers = Number((this.loggings.activeSeeker / (this.loggings.activeSeeker + this.loggings.inactiveSeeker) * 100).toFixed(2));
+  }
+  getPercentageActiveCA() {
+    this.percentageActiveCA = Number((this.loggings.activeCA / (this.loggings.activeCA + this.loggings.inactiveCA) * 100).toFixed(2));
+  }
+  getPercentageActiveHRM() {
+    this.percentageActiveHRM = Number((this.loggings.activeHRM / (this.loggings.activeHRM + this.loggings.inactiveHRM) * 100).toFixed(2));
+  }
+  getPercentageActiveHRA() {
+    this.percentageActiveHRA = Number((this.loggings.activeHRA / (this.loggings.activeHRA + this.loggings.inactiveHRA) * 100).toFixed(2));
   }
 }
