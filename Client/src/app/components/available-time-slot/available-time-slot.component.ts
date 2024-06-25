8658504081db504dc58cbff8c344cb2a078826f5
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2024-06-25 11:57:16 +0530

    new available time

--- Client/src/app/components/available-time-slot/available-time-slot.component.ts
+++ Client/src/app/components/available-time-slot/available-time-slot.component.ts
@@ -1,13 +1,44 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatCalendarBody } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCalendar } from '@angular/material/datepicker';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-available-time-slot',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,CommonModule, MatSidenavModule, MatCardModule, MatCalendarBody, MatNativeDateModule, MatCalendar],
   providers: [],
   templateUrl: './available-time-slot.component.html',
   styleUrls: ['./available-time-slot.component.css']
 })
-export class AvailableTimeSlotComponent{
+export class AvailableTimeSlotComponent {
+  selectedDate: Date = new Date();
+  calendarLoaded: boolean = false;
+  events = [
+    {
+      title: '(Lab) Database Management Systems',
+      time: '8:15 AM – 10:15 AM'
+    },
+    {
+      title: 'Fundamentals of Business Economics',
+      time: '10:30 AM – 12:30 PM'
+    },
+    {
+      title: 'Statistical Inference',
+      time: '3:30 PM – 5:30 PM'
+    }
+  ];
 
+  ngOnInit() {
+    // Delay loading of the calendar to avoid hydration issues
+    setTimeout(() => {
+      this.calendarLoaded = true;
+    }, 100);
+  }
 }
