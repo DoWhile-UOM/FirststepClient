1a6ef2cf8ff292b63a192bea7b3d5cd13ea8e551
RuwandieD <dharmapalard.21@uom.lk>
2024-07-03 11:59:02 +0530

    structure halfway done

--- Client/src/app/components/company-admin-dashboard/company-admin-dashboard.component.ts
+++ Client/src/app/components/company-admin-dashboard/company-admin-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-company-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatCard],
   templateUrl: './company-admin-dashboard.component.html',
   styleUrl: './company-admin-dashboard.component.css'
 })
--- Client/src/app/routes/company-admin.routes.ts
+++ Client/src/app/routes/company-admin.routes.ts
@@ -8,6 +8,7 @@ import { RoleProfileEditComponent } from '../components/role-profile-edit/role-p
 import { HrManagerApplicationListingComponent } from '../components/hr-manager-application-listing/hr-manager-application-listing.component';
 import { HrmanagerApplicationViewComponent } from '../components/hrmanager-application-view/hrmanager-application-view.component';
 import { SeekerProfileViewComponent } from '../components/seeker-profile-view/seeker-profile-view.component';
+import { CompanyAdminDashboardComponent } from '../components/company-admin-dashboard/company-admin-dashboard.component';
 
 export const caRoutes: Routes = [
   { path: '', redirectTo: 'jobOfferList', pathMatch: 'full' },
@@ -20,5 +21,6 @@ export const caRoutes: Routes = [
   { path: 'jobOfferList/applicationList/applicationView', component: HrmanagerApplicationViewComponent },
   { path: 'companyProfile', component: CompanyProfileEditComponent },
   { path: 'editRoleProfile', component: RoleProfileEditComponent },
-  { path: 'profile-view', component: SeekerProfileViewComponent }
+  { path: 'profile-view', component: SeekerProfileViewComponent },
+  { path: 'dashboard', component: CompanyAdminDashboardComponent}
 ];
