cc02b1a6eb51c20bd190dcb851b0e0853ba4920e
RuwandieD <dharmapalard.21@uom.lk>
2024-07-03 11:36:37 +0530

    CA dashboard created

---
+++ Client/src/app/components/company-admin-dashboard/company-admin-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-admin-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-admin-dashboard.component.html',
+  styleUrl: './company-admin-dashboard.component.css'
+})
+export class CompanyAdminDashboardComponent {
+
+}
