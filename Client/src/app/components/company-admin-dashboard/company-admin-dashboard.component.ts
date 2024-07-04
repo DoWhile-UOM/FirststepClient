5482ad02c8fff06b516840e23fd9901263c09f93
NethmaKarunathilaka <karunathilakanethma1@gmail.com>
2024-07-04 13:53:26 +0530

    divide app into sections

--- Client/src/app/components/company-admin-dashboard/company-admin-dashboard.component.ts
+++ Client/src/app/components/company-admin-dashboard/company-admin-dashboard.component.ts
@@ -5,13 +5,15 @@ import { LineGraphComponent } from "../line-graph/line-graph.component";
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { DoughnutGraphStatusComponent } from "../doughnut-graph-status/doughnut-graph-status.component";
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @Component({
     selector: 'app-company-admin-dashboard',
     standalone: true,
     templateUrl: './company-admin-dashboard.component.html',
     styleUrl: './company-admin-dashboard.component.css',
-    imports: [MatCardModule, MatCard, LineGraphComponent, MatButtonModule, MatIconModule, DoughnutGraphStatusComponent]
+    imports: [MatCardModule, MatCard, LineGraphComponent, MatButtonModule, MatIconModule, DoughnutGraphStatusComponent,MatMenuModule]
 })
 export class CompanyAdminDashboardComponent {
 
