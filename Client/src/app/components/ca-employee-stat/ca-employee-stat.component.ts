60cbc8509c707e73be2c463288eba76a9ea25c1d
RuwandieD <dharmapalard.21@uom.lk>
2024-07-04 15:09:32 +0530

    employee stat created

---
+++ Client/src/app/components/ca-employee-stat/ca-employee-stat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ca-employee-stat',
+  standalone: true,
+  imports: [],
+  templateUrl: './ca-employee-stat.component.html',
+  styleUrl: './ca-employee-stat.component.css'
+})
+export class CaEmployeeStatComponent {
+
+}
